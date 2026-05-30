1c98847c808898731097b9910ccaec8b0ba71b7f
macedera <acederamichael07@gmail.com>
2026-05-30 21:31:14 +0800

    feat(): created TodoItem component

---
+++ src/lib/components/TodoItem.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+   const {todo, deadline} = $props()
+</script>
+
+<div>
+    <div>
+        <h4>{todo}</h4>
+    </div>
+    <div>
+        <p>Deadline: <span>{new Date(deadline!).toLocaleDateString('en-GB')}</span></p>
+    </div>
+</div>
