aa030c16171035d505fff8cbebfd452dd6741336
macedera <acederamichael07@gmail.com>
2026-06-06 18:36:34 +0800

    feat(): able to show add todo div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 import TodoItem from "$lib/components/TodoItem.svelte";
-    const todos = [{ todo: "Cleaning", deadline: new Date('2/23/2025')}]
+    const todos = $state([{ todo: "Cleaning", deadline: new Date('2/23/2025')}])
+    let showAddTodo = $state(false)
+
+    function addTodo(){
+        todos.push();
+    }
+
+    function showAddTodoFunction(){
+        showAddTodo = !showAddTodo
+    }
 </script>
 
 <div>
+    <div>
+        <button onclick={showAddTodoFunction}>Add todo</button>
+    </div>
+{#if showAddTodo}
+	<div>
+        <input type="text">
+        <input type="date" name="" id="">
+        <button>Add</button>
+    </div>
+{/if}
 {#each todos as todo (todo)}
     <TodoItem todo={todo.todo} deadline={todo.deadline}/>
 {/each}
